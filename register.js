1fd5d302e03c6b43e788fdd736b1d9df38184869
Mohamed Chaieb <mohamedchaieb151@gmail.com>
2025-12-09 22:02:41 +0100

    Update register.js

--- register.js
+++ register.js
@@ -8,37 +8,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     form.addEventListener("submit", function (e) {
 
+        e.preventDefault();
+
         const name = nameInput.value.trim();
         const email = emailInput.value.trim();
         const password = passwordInput.value.trim();
         const confirmPassword = confirmPasswordInput.value.trim();
 
         const nameRegex = /^[A-Za-z]+$/;
+
         if (!nameRegex.test(name)) {
             alert("Name must contain alphabetic letters only.");
-            e.preventDefault();
             return;
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             alert("Enter a valid email address.");
-            e.preventDefault();
             return;
         }
 
         if (password.length < 6) {
             alert("Password must be at least 6 characters.");
-            e.preventDefault();
             return;
         }
 
         if (password !== confirmPassword) {
             alert("Passwords do not match.");
-            e.preventDefault();
             return;
         }
 
+        window.location.href = "main.html";
     });
-
 });
